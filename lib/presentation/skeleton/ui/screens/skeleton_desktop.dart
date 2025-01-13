db99423529b205df41d847c52c98a1fed647e404
namng <nllsover8.0@gmail.com>
2025-01-13 14:04:44 +0700

    chore: disble bell switch

--- lib/presentation/skeleton/ui/screens/skeleton_desktop.dart
+++ lib/presentation/skeleton/ui/screens/skeleton_desktop.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
